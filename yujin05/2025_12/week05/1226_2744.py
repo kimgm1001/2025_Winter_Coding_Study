6d88f5d3519dc6fb34b1346bcc0e4abfb69775f6
YUJIN DO <ureal@g.cnu.ac.kr>
2025-12-27 05:30:33 +0900

    Feat: Solve 10699 2744 10951 7287
    solve 10699 and modify 2744 10951 7287

--- yujin05/2025_12/week05/1226_2744.py
+++
@@ -1,7 +0,0 @@
-def main():
-    
-    s = input().strip()
-    print(s.swapcase())
-    
-if __name__ == '__main__':
-    main()
--- yujin05/2025_12/week05/1226_7287.py
+++
@@ -1,7 +0,0 @@
-def main():
-
-    print(43)
-    print("ureal")
-    
-if __name__ == '__main__':
-    main()
