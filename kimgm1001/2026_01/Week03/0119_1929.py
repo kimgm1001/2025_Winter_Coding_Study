597c88c5e16a34e62e5c82bd391bd4de7914bc6b
kimgm1001 <kimgm1001@gmail.com>
2026-01-21 13:42:32 +0900

    Feat: Solve 1929

---
+++ kimgm1001/2026_01/Week03/0119_1929.py
@@ -0,0 +1,22 @@
+## 에라토스테네스의 체: 소수만 걸러낼 때 효율적인 방법
+## pop: 빼내고, 한 칸씩 값을 옮겨가는 과정 필요 - O(N)
+## Boolean: 그냥 값만 덮어씌우기 - O(1)
+
+import sys
+
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+
+arr = [False, False] + [True] * (n - 1)
+
+for temp in range(2, int(n**0.5) + 1):
+    if arr[temp] == True:
+        for j in range(temp * 2, n + 1, temp):
+            arr[j] = False
+
+for i in range(m, n + 1):
+    if arr[i] == True:
+        print(i)
+
+## 풀이가 존나 높다..
